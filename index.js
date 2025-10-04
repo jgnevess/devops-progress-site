4f40369409ca533bb897685ca4d1397310a06c35
Joao Gabriel <joaogabriel443@gmail.com>
2025-10-04 18:25:23 -0300

    Automated commit 2025-10-04 18:25:23

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const checks = document.querySelectorAll('input[type="checkbox"]');
+
+  const concluidos = [1, 2, 3];
+
+  checks.forEach((checkbox) => {
+    const id = parseInt(checkbox.id, 10);
+    checkbox.checked = concluidos.includes(id);
+
+    checkbox.addEventListener('click', e => e.preventDefault());
+  });
+});
+
+const html = document.documentElement;
+const icon = document.getElementById("themeIcon");
+
+document.getElementById("toggleTheme").addEventListener("click", () => {
+  const atual = html.getAttribute("data-bs-theme");
+  const novo = atual === "dark" ? "light" : "dark";
+  html.setAttribute("data-bs-theme", novo);
+  icon.className = novo === "dark" ? "bi bi-sun" : "bi bi-moon";
+});;
